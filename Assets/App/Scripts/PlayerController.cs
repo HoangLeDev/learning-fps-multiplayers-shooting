767385a43ef3f6271260de7383727ee6f3c5849c
Johnny Le <Johnny.le@cuckookoo.vn>
2022-10-17 17:48:08 +0700

    Update

--- Assets/App/Scripts/PlayerController.cs
+++ Assets/App/Scripts/PlayerController.cs
@@ -14,7 +14,14 @@ public class PlayerController : MonoBehaviour
     {
         mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
 
+        //Quaternion property: A pack of x,y,z,w, one value change will affect the others
+        //Unity rotate by Quaternion, then need to use eulerAngles to force Quaternion by x,y,z rotation
+        //Otherwise, change rotation only by x,y,z cannot perform the true behavior because of Quaternion property
         transform.rotation =
             Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y + mouseInput.x, transform.eulerAngles.z);
+
+        viewPoint.rotation =
+            Quaternion.Euler(viewPoint.rotation.eulerAngles.x - mouseInput.y, viewPoint.rotation.eulerAngles.y,
+                viewPoint.rotation.eulerAngles.z);
     }
 }
