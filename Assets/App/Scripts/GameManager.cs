314f9f73616194680c2955ac2e85f907f3d495bd
JohnnyLe <johnny.le@cuckookoo.vn>
2022-12-01 16:25:54 +0700

    2.15 hide or show cursor on Build version

--- Assets/App/Scripts/GameManager.cs
+++ Assets/App/Scripts/GameManager.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
-    // Update is called once per frame
     void Update()
     {
-        
+        GameInputSetting();
     }
+
+    private void GameInputSetting()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else if (Cursor.lockState == CursorLockMode.None)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    } 
 }
