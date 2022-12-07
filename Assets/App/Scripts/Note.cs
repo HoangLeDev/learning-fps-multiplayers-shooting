13557770d0514f1408da8bbf8414450bd78705a5
Johnny Le <Johnnydev.box@gmail.com>
2022-12-08 00:26:28 +0700

    Add new Idea to Note

--- Assets/App/Scripts/Note.cs
+++ Assets/App/Scripts/Note.cs
@@ -1,10 +1,10 @@
 /* PlayerShooting.cs:
- * 1. Use Spawnpool asset to Spawn bullet impact for optimize
- * 2. 
+ * 1. Use SpawnPool asset to Spawn bullet impact for optimize
+ * 2. Change weapon overheat to out of Ammo
 */
 
 /* PlayerController.cs:
- * 1. 
+ * 1. While crunching under the block, user still can release the crunch and stuck into the block => need to fix
  * 2. 
 */
 
