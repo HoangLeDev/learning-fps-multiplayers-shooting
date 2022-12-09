135103ea7b9015e54ef33cde8f3c2367fd3e4eae
JohnnyLe <johnny.le@cuckookoo.vn>
2022-12-09 16:47:15 +0700

    Add new note Idea

--- Assets/App/Scripts/Note.cs
+++ Assets/App/Scripts/Note.cs
@@ -5,6 +5,6 @@
 
 /* PlayerController.cs:
  * 1. While crunching under the block, user still can release the crunch and stuck into the block => need to fix
- * 2. 
+ * 2. Add Animation while Switch gun or Add Text Show When Changing gun 
 */
 
