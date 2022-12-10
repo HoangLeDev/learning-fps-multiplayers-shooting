25a6dfb3090b9fbaced664ab7e4286ff4d4a4f21
Johnny Le <Johnnydev.box@gmail.com>
2022-12-10 13:25:37 +0700

    Add Demo animation, Add Connect to Master Server

--- Assets/App Networking/Network Scripts/Launcher.cs
+++ Assets/App Networking/Network Scripts/Launcher.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,17 +10,27 @@ public class Launcher : SingletonNetworking<Launcher>
     public GameObject loadingScreen;
     public GameObject menuBtns;
     public TMP_Text loadingTMP;
+    public GameObject demoGun;
 
     public void Start()
     {
         CloseMenu();
         loadingScreen.SetActive(true);
         loadingTMP.text = ConstantHolder.MESSAGE_CONNECT_TO_SERVER + "...";
+
+        PhotonNetwork.ConnectUsingSettings();
     }
-    
+
+    public override void OnConnectedToMaster()
+    {
+        CloseMenu();
+        menuBtns.SetActive(true);
+    }
+
     private void CloseMenu()
     {
         loadingScreen.SetActive(false);
         menuBtns.SetActive( false);
     }
+    
 }
