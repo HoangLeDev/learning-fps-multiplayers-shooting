aee1f595916a359af9c53cc7a2935668604d3a9e
Johnny Le <Johnnydev.box@gmail.com>
2022-12-10 19:16:20 +0700

    Update Create room, font, Constant
    - Update Create room function
    - Reset Default font
    - Add create room constant

--- Assets/App Networking/Network Scripts/Launcher.cs
+++ Assets/App Networking/Network Scripts/Launcher.cs
@@ -3,12 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
+using WebSocketSharp;
 
 public class Launcher : SingletonNetworking<Launcher>
 {
     public GameObject loadingScreen;
     public GameObject menuBtns;
+
+    public GameObject createRoomScreen;
+    public TMP_InputField roomNameInput;
+    
     public TMP_Text loadingTMP;
     public GameObject demoGun;
 
@@ -33,10 +39,31 @@ public class Launcher : SingletonNetworking<Launcher>
         menuBtns.SetActive(true);
     }
 
+    public void OpenRoomCreateChoice()
+    {
+        CloseMenu();
+        createRoomScreen.SetActive(true);
+    }
 
     private void CloseMenu()
     {
         loadingScreen.SetActive(false);
         menuBtns.SetActive(false);
+        createRoomScreen.SetActive(false);
+    }
+
+    public void StartCreateRoom()
+    {
+        if (!string.IsNullOrEmpty(roomNameInput.text))
+        {
+            RoomOptions options = new RoomOptions();
+            options.MaxPlayers = 8;
+            
+            PhotonNetwork.CreateRoom(roomNameInput.text, options);
+            
+            CloseMenu();
+            loadingTMP.text = ConstantHolder.MESSAGE_CREATING_ROOM;
+            loadingScreen.SetActive(true);
+        }
     }
 }
--- Assets/App/App Scripts/Utilities/ConstantHolder.cs
+++ Assets/App/App Scripts/Utilities/ConstantHolder.cs
@@ -8,4 +8,5 @@ public static class ConstantHolder
     public const string MESSAGE_CONNECT_TO_SERVER = "Connecting to Server...";
     public const string MESSAGE_LOADING = "Loading...";
     public const string MESSAGE_JOIN_LOBBY = "Joining to Lobby...";
+    public const string MESSAGE_CREATING_ROOM = "Creating Room...";
 }
